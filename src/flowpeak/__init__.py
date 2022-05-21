46612ed2b57ebe037487dc7880c18092b1064a34
OwEn-GaTO <marianolopezhoy@hotmail.com>
2022-05-20 23:30:32 -0500

    progress bar function added

--- src/flowpeak/__init__.py
+++ src/flowpeak/__init__.py
@@ -29,9 +29,11 @@ class Style:
 	NORMAL = Style.NORMAL
 	RESET_ALL = Style.RESET_ALL
 class console:
-	def __init__(self):
+	def __init__(self,margin=""):
 		self.ram = []
 		self.clear = ""
+		self.margin = margin
+		self.onprogress = False
 		if platform.system() == "Windows":
 			self.clear = "cls"
 		else:
@@ -39,21 +41,33 @@ class console:
 	def update(self):
 		os.system(self.clear)
 		for i in self.ram:
-			print(i,end = "") 
-	def print(self,msg,end = "\n",color = ""):
-	
-		self.ram.append(color + msg + end)
+			print(i,end = "")
+	def print(self,color = "",msg="",end = "\n",__cancelonprogress__=False):
+		if self.onprogress and __cancelonprogress__ == False:
+			self.ram.insert(-1,color + self.margin + msg + end)
+		else:
+			self.ram.append(color + self.margin + msg + end)
 		self.update()
 	def erase_line(self,line):
 		try:
 			self.ram.pop(line)
-			self.update()	
+			self.update()
 		except Exception as e:
-			raise e	
+			raise e
 	def erase_screen(self):
 		self.ram = []
 		self.update()
-
-
-
-
+	def progress_bar(self,iteration, total, decimals = 1, length = 100, printEnd = "\r"):
+		if self.onprogress:
+			self.erase_line(-1)
+		prefix = 'Progress:'
+		suffix = 'Complete'
+		fill = '█'
+		percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+		filledLength = int(length * iteration // total)
+		bar = fill * filledLength + '-' * (length - filledLength)
+		self.print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd,__cancelonprogress__ = True)
+		self.onprogress = True
+		if iteration == total:
+			self.onprogress = False
+			print()
